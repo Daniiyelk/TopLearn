7559b61c8e8c78a623d61e0ef25c94976c7631d0
daniiyel k <daniiyelkazemi666666@gmail.com>
2022-10-28 20:33:49 +0330

    committt

---
+++ Ticketing/Ticketing.DataLayer/Context/TicketingContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketing.DataLayer.Context
+{
+    public class TicketingContext : DbContext
+    {
+        public TicketingContext(DbContextOptions<TicketingContext> options):base(options)
+        {
+
+        }
+
+    }
+}
